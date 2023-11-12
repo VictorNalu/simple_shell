55f95857acc15bd1af0727628caf000d79986da3
veera-2 <naveera.nh@gmail.com>
2023-11-13 00:58:48 +0100

    main files

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth:hadiza hamisu,Victor Onyebueke
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+
+#endif /*MAIN_H*/
+
